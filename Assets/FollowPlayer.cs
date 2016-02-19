4e9bdd8a2d05f8b4ac7e6357ea43ad0e7aba6843
MakotoKanai <template_b_pepper@yahoo.co.jp>
2016-02-19 09:00:18 +0900

    finish tutorial 4

---
+++ Assets/FollowPlayer.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public Transform target;
+    public Vector3 offset;
+
+    void Start ()
+    {
+        // 自分自身と target との相対距離を求める
+        offset = GetComponent<Transform>().position - target.position;
+    }
+
+    void Update ()
+    {
+        // 自分の座標に target の座標を代入する
+        GetComponent<Transform>().position = target.position + offset;
+	}
+}
