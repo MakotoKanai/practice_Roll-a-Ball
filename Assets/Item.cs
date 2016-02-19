ab5cfa46235d6cfcef8e82c5629b0a8e3f035320
MakotoKanai <template_b_pepper@yahoo.co.jp>
2016-02-19 10:30:27 +0900

    finish tutorial 5

---
+++ Assets/Item.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Item : MonoBehaviour
+{
+    // トリガーとの接触時に呼ばれるコールバック
+    void OnTriggerEnter (Collider Hit)
+    {
+	    // 接触対象は Player タグ？
+        if (Hit.CompareTag("Player"))
+        {
+            // このコンポーネントを持つ GameObject を破棄する
+            Destroy(gameObject);
+        }
+	}
+}
